0132bd6377c5bdd8b0eb2a95a65e192d1f3057a0
Aleksa Raicevic <aleksa.raicevic97@gmail.com>
2018-07-24 14:39:16 +0200

    Inital Commit with FPS Content

---
+++ Source/ProvingGrounds/ProvingGroundsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProvingGroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProvingGroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProvingGroundsGameMode();
+};
+
+
+
